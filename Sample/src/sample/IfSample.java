674fb4d692fa837544865307b6008f95076c0341
ren.masuda.c936@dirbato.co.jp <RenMasuda()@Dirbato-1935X>
2025-05-23 17:25:04 +0900

    lesson2

---
+++ Sample/src/sample/IfSample.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class IfSample {
+
+    public static void main(String[] args) {
+        var score=50;
+        
+        if(score>=60)
+            System.out.println("合格です！");
+        else {
+            System.out.println("赤点です。。。");
+        }
+
+    }
+
+}
